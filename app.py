c26bd596c4d447844e16722844d35d0a5de00891
Bobiz2023 <127542727+Bobiz2023@users.noreply.github.com>
2024-08-18 15:50:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 from flask import Flask, render_template, jsonify
-from flask_basicauth import BasicAuth
 import server
 import subprocess
 
 app = Flask(__name__)
-
-# إعدادات المصادقة
-app.config['BASIC_AUTH_USERNAME'] = 'admin'
-app.config['BASIC_AUTH_PASSWORD'] = 'password'
-basic_auth = BasicAuth(app)
 
 # جلب عنوان IP العام باستخدام curl
 def get_public_ip():
@@ -16,30 +10,25 @@ def get_public_ip():
     return public_ip
 
 @app.route('/')
-@basic_auth.required
 def index():
     return render_template('index.html', status=server.get_server_status(), ip_address=get_public_ip())
 
 @app.route('/start')
-@basic_auth.required
 def start():
     server.start_server()
     return jsonify({'status': 'تم تشغيل السيرفر'})
 
 @app.route('/stop')
-@basic_auth.required
 def stop():
     server.stop_server()
     return jsonify({'status': 'تم إيقاف السيرفر'})
 
 @app.route('/restart')
-@basic_auth.required
 def restart():
     server.restart_server()
     return jsonify({'status': 'تم إعادة تشغيل السيرفر'})
 
 @app.route('/status')
-@basic_auth.required
 def status():
     return jsonify({'status': server.get_server_status()})
 
